d18ff42b0557ca37350e91dec92f20cb5abe69a9
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2023-11-02 14:54:33 +0430

    add linters

--- my_list.rb
+++ my_list.rb
@@ -2,11 +2,11 @@ require_relative 'my_enumerable'
 
 class MyList
   include MyEnumerable
-     def initialize(*elements)
-      @list = elements
-     end
+  def initialize(*elements)
+    @list = elements
+  end
 
-     def each(&block)
-      @list.each(&block)
+  def each(&block)
+    @list.each(&block)
   end
 end
